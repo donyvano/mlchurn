49833dfd45864bdf54417aa7cb5a3e07fcc59f5b
donyvano <yvan.nedelec.2703@gmail.com>
2026-04-18 13:47:17 +0200

    Add __init__.py to define dashboard page exports

---
+++ dashboard/pages/__init__.py
@@ -0,0 +1,8 @@
+from dashboard.pages import (
+    overview_page,
+    predictions_page,
+    monitoring_page,
+    experiments_page,
+)
+
+__all__ = ["overview_page", "predictions_page", "monitoring_page", "experiments_page"]
